58100007a085b1f6834bcdb78e0bdc1bf5d84706
student <student@vlab.local>
2024-04-04 16:22:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+    abstract class Animal{
+       public String name;
+        String age;
+        String weight;
+
+        public abstract void eat();
+            public abstract void voice();
+
+
+    }
+
+
+
+
---
+++ src/Mammal.java
@@ -0,0 +1,10 @@
+public class Mammal {
+
+    String species;
+    public Spec(String Name, String Age, String Weight) {
+       name = Name;
+       Age= age;
+       Weight= weight;
+    }
+
+}
